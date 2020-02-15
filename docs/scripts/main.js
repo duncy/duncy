bae6063f2e9efc763e418940edda23bb975da4fe
Duncan Fryer <35885610+alt1f923@users.noreply.github.com>
2020-02-15 17:58:00 +1300

    fix issue with text being visible before javascript loads
    header text is now a vue component

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,3 +1,7 @@
+Vue.component('header-text', {
+    template: '<transition name="fade" appear><div><h1>Duncan<br>Fryer</h1><div class=line></div><h2>[ Дункан Фрайер ]</h2></div></transition>'
+})
+
 var header = new Vue({
     el: 'header'
 })
